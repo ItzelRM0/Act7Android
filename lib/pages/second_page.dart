8994251d84dca64e93d91de2acd0f8d7cbe89bcc
FlutLab.io <>
2021-06-08 05:39:46 +0000

    Initial commit

---
+++ lib/pages/second_page.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cuentas'),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.bottomRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Colors.greenAccent,
+                Colors.greenAccent,
+                Colors.greenAccent
+              ],
+            ),
+          ),
+        ),
+      ), //AppBar
+      body: SingleChildScrollView(
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              _crearCard1(),
+              SizedBox(
+                height: 10,
+              ),
+              _crearInput1(),
+              SizedBox(
+                height: 10,
+              ),
+              _crearInput2(),
+              SizedBox(
+                height: 10,
+              ),
+              FlatButton(
+                onPressed: () {},
+                color: Colors.greenAccent,
+                child: Text(
+                  'Enviar',
+                  style: TextStyle(color: Colors.black),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _crearInput1() {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: TextFormField(
+        decoration: InputDecoration(
+            labelText: 'Email',
+            icon: Icon(
+              Icons.email,
+              color: Colors.greenAccent,
+            ),
+            hintText: 'Email'),
+      ),
+    );
+  }
+
+  Widget _crearInput2() {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: TextFormField(
+        obscureText: true,
+        decoration: InputDecoration(
+            labelText: 'Password',
+            icon: Icon(
+              Icons.https,
+              color: Colors.greenAccent,
+            ),
+            hintText: 'Password'),
+      ),
+    );
+  }
+
+  Widget _crearCard1() {
+    return Card(
+      elevation: 5,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      child: Container(
+        child: Column(
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.only(topRight: Radius.circular(20), topLeft: Radius.circular(20), bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20)),
+              child: FadeInImage(
+                placeholder: AssetImage('assets/inicio.gif'),
+                image: NetworkImage("https://raw.githubusercontent.com/ItzelRM0/MisImagenes7/main/inicio.gif"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+} // Second page
---
+++ lib/pages/third_page.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+class ThirdPage extends StatefulWidget {
+  @override
+  _ThirdPageState createState() => _ThirdPageState();
+}
+
+const List<Color> greenGradients = [
+  Color(0xFF7700FF),
+  Color(0xFF8351D4),
+  Color(0xFF764C8A),
+]; //Fin const
+const List<Color> lightGreenGradients = [
+  Color(0xFF76FF03),
+  Color(0xFF64FFDA),
+  Color(0xFFB2FF59),
+]; //Fin const
+const List<Color> limeGradients = [
+  Color(0xFF0F13F8),
+  Color(0xFF5570C7),
+  Color(0xFF5E8EF7),
+]; //Fin const
+
+class _ThirdPageState extends State<ThirdPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        child: ConstrainedBox(
+      constraints: BoxConstraints(maxWidth: 300, minWidth: 200),
+      child: SingleChildScrollView(
+          child: Column(children: <Widget>[
+        SizedBox(
+          height: 20.0,
+        ),
+
+        Container(
+            height: 100,
+            width: 250,
+            decoration: ShapeDecoration(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+              gradient: LinearGradient(colors: greenGradients),
+            ), //ShapeDecoration
+            child: Container(
+              padding: EdgeInsets.only(top: 25.0, bottom: 16),
+              child: Text('Productos del día', textAlign: TextAlign.center, style: TextStyle(color: Colors.black, fontSize: 25.0, fontWeight: FontWeight.bold) //Fin TextStyle
+                  ),
+            ) //Fin Container
+            ), //Fin Container2
+              
+        SizedBox(
+          height: 20.0,
+        ),
+        Container(
+          height: 100,
+          width: 175,
+          alignment: Alignment.center,
+          decoration: ShapeDecoration(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+            gradient: LinearGradient(colors: limeGradients),
+          ), //ShapeDecoration
+
+          child: Text('Paquetes disponibles:', textAlign: TextAlign.center, style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold) //Fin TextStyle
+              ),
+        ), //Fin Container 3
+
+              SizedBox(
+          height: 20.0,
+        ),
+        Container(
+          height: 100,
+          width: 175,
+          alignment: Alignment.center,
+          decoration: ShapeDecoration(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+            gradient: LinearGradient(colors: lightGreenGradients),
+          ), //ShapeDecoration
+
+          child: Text('Paquete 1', textAlign: TextAlign.center, style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold) //Fin TextStyle
+              ),
+        ), //Fin Container 3
+              
+        SizedBox(
+          height: 20.0,
+        ),
+        InkWell(
+          onTap: () {
+            print('Paquete 1');
+          },
+          child: Container(padding: EdgeInsets.all(5), child: Image.asset("assets/images/carro2.jpg")), //Fin Container 4
+        ), //fin inkwell
+              
+              SizedBox(
+          height: 20.0,
+        ),
+        Container(
+          height: 100,
+          width: 175,
+          alignment: Alignment.center,
+          decoration: ShapeDecoration(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+            gradient: LinearGradient(colors: lightGreenGradients),
+          ), //ShapeDecoration
+
+          child: Text('Paquete 2', textAlign: TextAlign.center, style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold) //Fin TextStyle
+              ),
+        ), //Fin Container 3
+              
+        SizedBox(
+          height: 20.0,
+        ),
+        InkWell(
+          onTap: () {
+            print('Paquete 2');
+          },
+          child: Container(padding: EdgeInsets.all(5), child: Image.asset("assets/images/carro3.jpg")), //Fin Container 4
+        ), //fin inkwell
+              
+              
+      ] //Fin Widget[]
+
+              ) //Fin Column
+          ), //Fin SingleChildScrollView
+    ) //Fin ConstrainedBox
+
+        ); //Fin Material
+  } // widget
+
+} //Fin clase _ThirdPage
